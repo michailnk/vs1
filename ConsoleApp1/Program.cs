403b0aab5dcce0c88e5846cde7118292ea37684d
myname <mikhail@n>
2021-01-11 18:09:55 +0200

    resolved mogifire after merge

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,12 +5,9 @@ namespace ConsoleApp1 {
         static void Main(string[] args) {
             // comment created to add git hub
             // code was added to VS
-<<<<<<< HEAD
-
-=======
             
             // code to call Fiature 1
->>>>>>> f20168ce16409792a9f3f462a2b5ea99b0826ff2
+
         }
     }
 }
