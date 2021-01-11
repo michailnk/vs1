04f92cba3877946f00bdf2b8d917faa9597441dd
michailnk <66944689+michailnk@users.noreply.github.com>
2021-01-11 13:19:57 +0200

    Update Program.cs
    add comment

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,7 +3,7 @@
 namespace ConsoleApp1 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            // comment created to add git hub
         }
     }
 }
